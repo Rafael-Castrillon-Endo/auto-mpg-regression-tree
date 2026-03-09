39961c4d7159c562a73ddf614b56413a7f9ffeaa
Rafael <rafaelgithub25@gmail.com>
2026-03-08 20:39:27 -0500

    Deleting comments in preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -9,7 +9,6 @@ def emtpy_values(data):
         for j in range(len(FEATURES)):
             if(row[FEATURES[j]] == '?'):
                 index.append({'index_value': i, 'col' : j})
-    #print(len(index))
     return index
 
 def get_avarage_features(data):
@@ -28,15 +27,9 @@ def get_avarage_features(data):
 
 def preprocess(data):
     index = emtpy_values(data)
-    #for i in range(len(index)):
-     #   row = data.iloc[index[i]['index_value']]
-      #  print(row)
-       # print("================================")
-    #print(f"size -> {len(index)}")
     if(len(index)):
         avarage = get_avarage_features(data)
         for i in range(len(index)):
-            #{'index_value': x, 'col' : x}
             index_value = index[i]['index_value']
             col = index[i]['col']
             data.at[index_value, FEATURES[col]] = (avarage[col])
