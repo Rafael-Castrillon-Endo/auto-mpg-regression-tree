e1992e79c3ec2e3758aa4c572f49c4e52d065f18
Rafael <rafaelgithub25@gmail.com>
2026-03-09 10:18:39 -0500

    main.py API

--- api/main.py
+++ api/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes import dataset
 
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(dataset.router)
 
