1f3d2d480b6eb3f37f773d5c24e1600b1251a83f
Rafael <rafaelgithub25@gmail.com>
2026-03-10 10:17:51 -0500

    delete.py

--- api/routes/delete.py
+++ api/routes/delete.py
@@ -6,3 +6,5 @@ def delete_row(index : int , request : Request):
     df = request.app.state.dataset
     if(index not in df):
         raise HTTPException (status_code = 404, detail = "index invalido")
+    df.drop(index = index, inplace = True)
+    return {"message" : "Dato eliminado"}
